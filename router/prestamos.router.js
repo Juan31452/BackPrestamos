28f270fc00cc22f232331b6fc4300513a47496ae
Juan Carlos <jcs1011@gmail.com>
2022-12-06 17:58:55 -0500

    se crea pestamos.router

---
+++ router/prestamos.router.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const PrestamoControl = require("../controlador/prestamos.control");
+
+router.post("/",PrestamoControl.create);
+router.get("/",PrestamoControl.find);
