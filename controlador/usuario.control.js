fdc02cb0dcdd286b9df2174bd299bc32b05dff72
Juan Carlos <jcs1011@gmail.com>
2023-05-03 21:20:35 -0500

    se agrega editar y listar usuario

--- controlador/usuario.control.js
+++ controlador/usuario.control.js
@@ -32,7 +32,7 @@ exports.create = function(req,res)
     })              
 }
 
-// Obtener todos los clientes
+// Obtener todos los usuarios
 exports.find = async (req, res) => {
    
       const usuario = await Usuario.find();
@@ -47,3 +47,40 @@ exports.find = async (req, res) => {
         res.json(usuarios)
     })
 }
+
+// Buscar usuario por id
+exports.findOne = function(req,res)
+{
+    Usuario.findOne({_id : req.params.id},function(err,usuario){
+        res.json(usuario)
+    })
+}
+
+//Modificar datos
+exports.update = function(req,res)
+{
+    let usuario = {
+        email: req.body.email,
+        password: req.body.password,
+        interes: req.body.interes,
+        
+    }
+
+    Usuario.findByIdAndUpdate(req.params.id,{$set :usuario},
+        function(err){
+        if(err)
+        {
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al intentar Modificar"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "Modificado con Exito"
+        res.json(response)
+           
+    })
+
+}    
--- router/usuario.router.js
+++ router/usuario.router.js
@@ -5,5 +5,7 @@ const UsuariosControl = require("../controlador/usuario.control");
 
 router.post("/",UsuariosControl.create);
 router.get("/",UsuariosControl.find);
-router.post("/login",UsuariosControl.login)
+router.post("/login",UsuariosControl.login);
+router.get("/:id",UsuariosControl.findOne);
+router.put("/:id",UsuariosControl.update);
 module.exports = router;
